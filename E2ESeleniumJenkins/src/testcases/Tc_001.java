cbfe1a09cfddc3d61d0517673ca02bb0f05a19f5
sangee5656 <gsangeetha05@gmail.com>
2020-05-02 08:59:46 +0530

    Create Automation project

---
+++ E2ESeleniumJenkins/src/testcases/Tc_001.java
@@ -0,0 +1,20 @@
+package testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Tc_001 {
+
+	@Test
+	public void testcase1()
+	{
+		System.setProperty("webdriver.chrome.driver","./Driver/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com");
+		driver.findElement(By.id("email")).sendKeys("Hello");
+		driver.findElement(By.id("pass")).sendKeys("ab123");
+		driver.quit();
+		
+	}
+}
